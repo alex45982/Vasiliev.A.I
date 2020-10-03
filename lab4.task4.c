d434539b187aedb401ae0a571b3ecaea1a403191
alex45982 <71696895+alex45982@users.noreply.github.com>
2020-10-03 17:46:28 +0300

    Update lab4.task4.c

--- lab4.task4.c
+++ lab4.task4.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 int main()
 {
-	int a = 20, b = 10;
+	int a, b;
+
+	printf("enter a:");
+	scanf_s("%i", &a);
+
+	printf("enter b:");
+	scanf_s("%i", &b);
+
 	int asq = a * a, bsq = b * b;
 	int summ = asq + bsq, diff = asq - bsq, mult = asq * bsq;
 	float del = (float)asq / bsq;
