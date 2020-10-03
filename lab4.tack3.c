e113201c5402dcae260801715e64140eda769cc2
alex45982 <71696895+alex45982@users.noreply.github.com>
2020-10-03 17:45:27 +0300

    Update lab4.tack3.c

--- lab4.tack3.c
+++ lab4.tack3.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 int main()
 {
-	int a = 10, b = 20;
+	int a, b;
+
+	printf("enter a:");
+	scanf_s("%i", &a);
+
+	printf("enter b:");
+	scanf_s("%i", &b);
+
 	float x = ((float)a + b) / 2;
 	printf("(a+b)/2=%f\n", x);
+	
 	return 0;
 }
