2c1ae7d07ff856165d6ce3b32a0c0afa6376676d
alex45982 <71696895+alex45982@users.noreply.github.com>
2020-10-03 17:47:29 +0300

    Update lab4.task5.c

--- lab4.task5.c
+++ lab4.task5.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 int main()
 {
-	int a = 20, b = -10;
+	int a, b;
+
+	printf("enter a:");
+	scanf_s("%i", &a);
+
+	printf("enter b:");
+	scanf_s("%i", &b);
+
 	int am = abs(a), bm = abs(b);
 	int summ = am + bm, diff = am - bm, mult = am * bm;
 	float del = (float)am / bm;
